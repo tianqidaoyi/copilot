208d30dd0639b17b8575ba49af3b4ee00e34be39
huang <48399297+tianqidaoyi@users.noreply.github.com>
2023-07-21 09:23:35 +0800

    commit code

---
+++ src/main/java/com/example/copilottest/CopilotTestApplication.java
@@ -0,0 +1,13 @@
+package com.example.copilottest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CopilotTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CopilotTestApplication.class, args);
+    }
+
+}
