f3ac02fa86675f31bf132da447e07de824e502e5
huang <48399297+tianqidaoyi@users.noreply.github.com>
2023-07-21 09:13:09 +0800

    init project

---
+++ src/test/java/com/example/copilottest/CopilotTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.copilottest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CopilotTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
